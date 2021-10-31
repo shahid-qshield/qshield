76b586aac032e7fd8c0fb65c49f81f8ab0ec759a
Maria L Soliman <ml.soliman@ever-bs.com>
2021-10-31 13:14:35 +0200

    [FIX] ebs_qshield_employee: removed updates

--- ebs_qshield_employee/__manifest__.py
+++ ebs_qshield_employee/__manifest__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': "QShield Module",
+
     'summary': """
         This module contains custom modifications for QSshield Employee Module
         """,
+
     'description': """
        This module contains custom modifications for QSshield Employee Module
     """,
+
     'author': "Maria L Soliman",
     'website': "http://www.ever-bs.com/",
     'category': 'Uncategorized',
@@ -14,9 +17,9 @@
     'depends': ['base', 'contacts', 'hr', 'hr_contract', 'documents', 'helpdesk', 'documents_hr_contract'],
     'data': [
         'security/ir.model.access.csv',
-        'views/contacts_view_custom.xml',
         'views/employee_view_custom.xml',
         'views/contracts_view_custom.xml',
+        'views/contacts_view_custom.xml',
         'reports/employee_information_form.xml',
     ],
 }
