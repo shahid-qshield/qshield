9eb2116572aa7c8ab128503484fe9ad87ec7c6c3
Bhavesh Parmar <bhaveshparmar.techultra@gmail.com>
2022-03-24 18:50:24 +0530

    [Fix]singleton error in unlink contract

--- qshield_payslip/models/hr_contract.py
+++ qshield_payslip/models/hr_contract.py
@@ -6,8 +6,9 @@ class HrContractInherit(models.Model):
     _inherit = 'hr.contract'
 
     def unlink(self):
-        payslips = self.env['qshield.payslip'].search_count([('contract_id', '=', self.id)])
-        if payslips:
-            raise UserError(
-                'You cannot delete a contract has payslips')
+        for record in self:
+            payslips = self.env['qshield.payslip'].search_count([('contract_id', '=', record.id)])
+            if payslips:
+                raise UserError(
+                    'You cannot delete a contract has payslips')
         return super(HrContractInherit, self).unlink()
