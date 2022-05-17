12da9adf1d8b97579542a473e1654a4f857bdd82
Bhavesh Parmar <bhaveshparmar.techultra@gmail.com>
2022-05-17 17:05:42 +0530

    [ADD]set number of days and hours base on contract in leave

--- ebs_leave_appilcation_request/models/hr_leave_custom.py
+++ ebs_leave_appilcation_request/models/hr_leave_custom.py
@@ -1,6 +1,8 @@
 from dateutil import relativedelta
 from odoo import models, fields, api
-from datetime import date, datetime
+from datetime import datetime
+from odoo.addons.resource.models.resource import HOURS_PER_DAY
+from pytz import UTC
 
 
 class HrEmployeeBase(models.AbstractModel):
@@ -41,6 +43,18 @@ class HRLeaveCustom(models.Model):
     total_number_of_approved_leave_days = fields.Float(string="get_total_days", default=0)
     hr_contract = fields.Many2one('hr.contract', related="employee_id.contract_id")
     is_approved = fields.Boolean(default=False)
+    leave_selection = fields.Selection(selection=[
+        ('working_days', 'Working Days'),
+        ('calendar_days', 'Calendar Days')],
+        compute="compute_leave_selection", store=True)
+
+    @api.depends('employee_id')
+    def compute_leave_selection(self):
+        for leave in self:
+            if leave.employee_id and leave.employee_id.contract_id:
+                leave.leave_selection = leave.employee_id.contract_id.leave_selection
+            else:
+                leave.leave_selection = False
 
     # @api.depends('total_days_approved')
     # @api.onchange('total_days_approved')
@@ -49,6 +63,26 @@ class HRLeaveCustom(models.Model):
     #         self.request_date_to = self.request_date_from + relativedelta.relativedelta(
     #             days=self.total_days_approved - 1)
     #         self.date_to = self.request_date_to
+
+    def _get_number_of_days(self, date_from, date_to, employee_id):
+        """ Returns a float equals to the timedelta between two dates given as string."""
+        if employee_id:
+            employee = self.env['hr.employee'].browse(employee_id)
+            if employee and employee.contract_id and employee.contract_id.leave_selection == 'calendar_days':
+                if date_from and date_to:
+                    days = (date_to - date_from).days + 1
+                    hours = days * 24
+                    if employee.contract_id.resource_calendar_id:
+                        hours = days * employee.contract_id.resource_calendar_id.hours_per_day
+                    elif employee.resource_calendar_id:
+                        hours = days * employee.resource_calendar_id.hours_per_day
+                    return {'days': days, 'hours': hours}
+            else:
+                if employee_id:
+                    return super(HRLeaveCustom, self)._get_number_of_days(date_from, date_to, employee_id)
+        else:
+            if employee_id:
+                return super(HRLeaveCustom, self)._get_number_of_days(date_from, date_to, employee_id)
 
     @api.model
     def create(self, vals):
@@ -63,6 +97,44 @@ class HRLeaveCustom(models.Model):
     #     print(self.requested_days_before_approve)
 
     # return super(HRLeaveCustom, self).write(vals)
+
+    @api.depends('number_of_days')
+    def _compute_number_of_hours_display(self):
+        for holiday in self:
+            calendar = holiday._get_calendar()
+            if holiday.date_from and holiday.date_to:
+                # Take attendances into account, in case the leave validated
+                # Otherwise, this will result into number_of_hours = 0
+                # and number_of_hours_display = 0 or (#day * calendar.hours_per_day),
+                # which could be wrong if the employee doesn't work the same number
+                # hours each day
+                if holiday.state == 'validate':
+                    if holiday.leave_selection == 'calendar_days':
+                        days = (holiday.date_to - holiday.date_from).days + 1
+                        number_of_hours = days * 24
+                        if holiday.employee_id.contract_id.resource_calendar_id:
+                            number_of_hours = days * holiday.employee_id.contract_id.resource_calendar_id.hours_per_day
+                        elif holiday.employee_id.resource_calendar_id:
+                            number_of_hours = days * holiday.employee_id.resource_calendar_id.hours_per_day
+                    else:
+                        start_dt = holiday.date_from
+                        end_dt = holiday.date_to
+                        if not start_dt.tzinfo:
+                            start_dt = start_dt.replace(tzinfo=UTC)
+                        if not end_dt.tzinfo:
+                            end_dt = end_dt.replace(tzinfo=UTC)
+                        resource = holiday.employee_id.resource_id
+                        intervals = calendar._attendance_intervals_batch(start_dt, end_dt, resource)[resource.id] \
+                                    - calendar._leave_intervals_batch(start_dt, end_dt, None)[
+                                        False]  # Substract Global Leaves
+                        number_of_hours = sum((stop - start).total_seconds() / 3600 for start, stop, dummy in intervals)
+                else:
+                    number_of_hours = \
+                        holiday._get_number_of_days(holiday.date_from, holiday.date_to, holiday.employee_id.id)['hours']
+                holiday.number_of_hours_display = number_of_hours or (
+                        holiday.number_of_days * (calendar.hours_per_day or HOURS_PER_DAY))
+            else:
+                holiday.number_of_hours_display = 0
 
     @api.depends('employee_id', 'holiday_status_id')
     @api.onchange('employee_id', 'holiday_status_id')
