3cf12ff6676ad25a5e3273031788f02ca6fdaebf
Bhavesh Parmar <bhaveshparmar.techultra@gmail.com>
2022-10-03 18:13:27 +0530

    [ADD]funcationaly of set price from pricelist in general sale order line wizard

--- qshield_crm/models/generate_sale_order_line.py
+++ qshield_crm/models/generate_sale_order_line.py
@@ -18,6 +18,8 @@ class GenerateSaleOrderLine(models.Model):
     total_consolidation_price = fields.Float(string="Total Price")
     calculated_total_consolidation_price = fields.Float(compute="compute_calculated_total_consolidation_price",
                                                         store=True)
+    set_price_type = fields.Selection([('manually', 'Manually'), ('price_list', 'Price list')], string="Set Price Type",
+                                      default='manually', required='True')
 
     # @api.onchange('total_consolidation_price')
     # def onchange_total_consolidation_price(self):
@@ -41,21 +43,71 @@ class GenerateSaleOrderLine(models.Model):
 
     def update_consolidation_price(self):
         print('----------update_consolidation_price--------------------')
+        if self.total_consolidation_price <= 0:
+            raise UserError('please set total price More than Zero')
         service_type_consolidation_manual_ids = self.service_type_consolidation_manual_ids.filtered(
             lambda s: s.is_set_automatic == False)
-        remaining_service_type_consolidation_manual_ids = self.service_type_consolidation_manual_ids - service_type_consolidation_manual_ids
-        total_price = self.total_consolidation_price
-        if self.total_consolidation_price > 0 and service_type_consolidation_manual_ids:
-            for service_type_consolidation_manual_id in service_type_consolidation_manual_ids:
-                service_type_consolidation_manual_id.sudo().write(
-                    {'price': total_price / len(service_type_consolidation_manual_ids)})
-        action = self.env.ref('qshield_crm.action_generate_sale_order_line').read()[0]
-        action.update({'res_id': self.id})
-        return action
+        if service_type_consolidation_manual_ids:
+            total_price = self.total_consolidation_price
+            if self.total_consolidation_price > 0 and service_type_consolidation_manual_ids:
+                for service_type_consolidation_manual_id in service_type_consolidation_manual_ids:
+                    service_type_consolidation_manual_id.sudo().write(
+                        {'price': total_price / len(service_type_consolidation_manual_ids)})
+            action = self.env.ref('qshield_crm.action_generate_sale_order_line').read()[0]
+            action.update({'res_id': self.id})
+            return action
+        else:
+            raise UserError('please add service consolidation')
+
+    def update_price_from_price_list(self):
+        if self.sale_order_id and self.sale_order_id.pricelist_id:
+            if not self.generate_service_type_variant_ids:
+                raise UserError('Service type variant must be necessary please generate service type variant first')
+            for generate_service_type_variant_id in self.generate_service_type_variant_ids:
+                product = generate_service_type_variant_id.variant_id.product_id
+                if product:
+                    price = product._get_tax_included_unit_price(
+                        self.sale_order_id.company_id or self.sale_order_id.company_id,
+                        self.sale_order_id.currency_id,
+                        self.sale_order_id.date_order,
+                        'sale',
+                        fiscal_position=self.sale_order_id.fiscal_position_id,
+                        product_price_unit=self._get_display_price(product, self.sale_order_id),
+                        product_currency=self.sale_order_id.currency_id
+                    )
+                    generate_service_type_variant_id.sudo().write({'price': price})
+                else:
+                    raise UserError('variant have not linked with product')
+            action = self.env.ref('qshield_crm.action_generate_sale_order_line').read()[0]
+            action.update({'res_id': self.id})
+            return action
+        else:
+            raise UserError('Please select price list in sale order')
+
+    def _get_display_price(self, product, sale_order):
+        if sale_order.pricelist_id.discount_policy == 'with_discount':
+            return product.with_context(pricelist=sale_order.pricelist_id.id, uom=False).price
+        product_context = dict(self.env.context, partner_id=sale_order.partner_id.id, date=sale_order.date_order,
+                               uom=False)
+        final_price, rule_id = sale_order.pricelist_id.with_context(product_context).get_product_price_rule(
+            product, 1.0, sale_order.partner_id)
+        base_price, currency = self.with_context(product_context)._get_real_price_currency(product, rule_id,
+                                                                                           1.0,
+                                                                                           False,
+                                                                                           sale_order.pricelist_id.id)
+        if currency != sale_order.pricelist_id.currency_id:
+            base_price = currency._convert(
+                base_price, sale_order.pricelist_id.currency_id,
+                sale_order.company_id or self.env.company, sale_order.date_order or fields.Date.today())
+        return max(base_price, final_price)
 
     def generate_sale_order_line(self):
         print('------------------------')
         if self.generate_service_type_variant_ids:
+            if self.set_price_type == 'manually':
+                self.update_consolidation_price()
+            elif self.set_price_type == 'price_list':
+                self.update_price_from_price_list()
             consolidation_list = []
             consolidation_ids = self.generate_service_type_variant_ids.mapped('variant_id').mapped('consolidation_id')
             for consolidation_id in consolidation_ids:
@@ -81,6 +133,8 @@ class GenerateSaleOrderLine(models.Model):
                     if key.price > 0 and len(values) > 0:
                         price = key.price / len(values)
                     for value in values:
+                        if self.set_price_type == 'price_list':
+                            price = value.price
                         self.sale_order_id.sudo().write({'order_line': [(0, 0, {
                             'product_id': value.variant_id.product_id.id,
                             # 'name': 'Variant :' + value.variant_id.name,
@@ -88,6 +142,8 @@ class GenerateSaleOrderLine(models.Model):
                             'price_unit': price
                         })]})
                 print('--------------')
+        else:
+            raise UserError('Please Generate service variant')
 
     def generate_service_variant(self):
         if not self.service_type_consolidation_manual_ids and self.generate_service_type_variant_ids:
@@ -121,9 +177,11 @@ class GenerateSaleOrderLine(models.Model):
                         self.write({
                             'generate_service_type_variant_ids': [(4, generate_service_type_variant_id.id)]
                         })
-        action = self.env.ref('qshield_crm.action_generate_sale_order_line').read()[0]
-        action.update({'res_id': self.id})
-        return action
+            action = self.env.ref('qshield_crm.action_generate_sale_order_line').read()[0]
+            action.update({'res_id': self.id})
+            return action
+        else:
+            raise UserError('Please Add service type Consolidation line')
 
 
 class ServiceTypeConsolidationManual(models.Model):
@@ -140,6 +198,7 @@ class ServiceTypeConsolidationManual(models.Model):
     def onchange_price(self):
         if self._context.get('price'):
             self.is_set_automatic = True
+
 
 class GenerateServiceTypeVariant(models.Model):
     _name = 'generate.service.type.variant'
